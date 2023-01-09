f3bfbf7d333b045338256645e3c1dd29ce3f8050
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-01-09 15:40:27 -0300

    number event, clear form

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -31,5 +31,36 @@ export function init(){
    state.errorCep = document.querySelector('[data-error="cep"]');
    state.errorNumber = document.querySelector('[data-error="number"]');
 
-   console.log(state);
+   state.inputNumber.addEventListener('change', handleInputNumberChange);
+   state.btnClear.addEventListener('click', handleBtnClearClick);
+}
+
+function handleInputNumberChange(event){
+    if(event.target.value == ""){
+        setFormError("number", "Campo requerido");
+    }else {
+        setFormError("number", "");
+    }
+}
+
+function handleBtnClearClick(event){
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm(){
+  state.inputCep.value = "";
+  state.inputCity.value = "";
+  state.inputNumber.value = "";
+  state.inputStreet.value = "";
+
+  setFormError("cep", "");
+  setFormError("number", "");
+
+  state.inputCep.focus();
+}
+
+function setFormError(key, value){
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
 }
